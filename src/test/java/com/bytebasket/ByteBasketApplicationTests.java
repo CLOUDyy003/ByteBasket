f5cbb7eab45421928bd8c7a804fd7f6401bbb38d
Cloudyy <sukarebhavesh@gmail.com>
2025-11-08 12:14:14 +0530

    Initial setup: Spring Boot, PostgreSQL, Security config, and test page

---
+++ src/test/java/com/bytebasket/ByteBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
